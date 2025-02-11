5a7d56031b47e9be743f29b048b6bd20c831a30a
lihuibear <lihuibear4@gmail.com>
2025-02-11 23:41:22 +0800

    添加项目基础框架及配置 通用基础代码:   自定义异常   响应包装类   全局异常处理器   请求包装类   全局跨域配置

---
+++ src/main/java/com/lihui/picturebackend/PictureBackendApplication.java
@@ -0,0 +1,18 @@
+package com.lihui.picturebackend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@MapperScan("com.lihui.picturebackend.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class PictureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureBackendApplication.class, args);
+    }
+
+}
